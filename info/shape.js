c3ddd50f8846d241e1859e61762e2c14dc35e692
tfkjosh <140848241+tfkjosh@users.noreply.github.com>
2023-11-12 21:23:47 -0600

    Delete info directory

--- info/shape.js
+++
@@ -1,9 +0,0 @@
-class Shape {
-    constructor(shapeColor, text, textColor) {
-        this.shapeColor = shapeColor;
-        this.text = text;
-        this.textColor = textColor;
-    }
-};
-
-module.exports = Shape;
